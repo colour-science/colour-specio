09fad38b2e2a40bab232ccfe883c8b653b500195
Tucker Downs <tucker@roevisual.com>
2022-10-19 22:18:25 -0700

    bump version

--- specio/__init__.py
+++ specio/__init__.py
@@ -3,7 +3,7 @@ Specio
 ======
 Provides support for interacting with various hardware spectrometers.
 """
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from .common import Measurement, RawMeasurement
 from .spectrometer import SpecRadiometer
