e2cb4ca7291a6fdc172d116cf8c46659c86531ec
Tucker Downs <tucker@roevisual.com>
2022-10-20 10:15:41 -0700

    no timer code in the example method

--- specio/fileio.py
+++ specio/fileio.py
@@ -70,25 +70,23 @@ def load_measurements(file: str) -> List[Measurement]:
     pass
 
 
-from time import time
-
-
-def timer_func(func):
-    # This function shows the execution time of
-    # the function object passed
-    def wrap_func(*args, **kwargs):
-        t1 = time()
-        result = func(*args, **kwargs)
-        t2 = time()
-        print(f"Function {func.__name__!r} executed in {(t2-t1):.4f}s")
-        return result
-
-    return wrap_func
-
-
 if __name__ == "__main__":
+    from time import time
+
+    def timer_func(func):
+        # This function shows the execution time of
+        # the function object passed
+        def wrap_func(*args, **kwargs):
+            t1 = time()
+            result = func(*args, **kwargs)
+            t2 = time()
+            print(f"Function {func.__name__!r} executed in {(t2-t1):.4f}s")
+            return result
+
+        return wrap_func
+
     measures = []
-    for i in range(10):
+    for i in range(100):
         measures.append(Measurement())
 
     file = "/Users/tucker/Downloads/test"
